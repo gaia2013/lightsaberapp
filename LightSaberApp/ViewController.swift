774122fd4e305f4cefdc57df2d2759a61b333189
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2018-08-05 15:28:37 +0900

    Initial Commit

---
+++ LightSaberApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LightSaberApp
+//
+//  Created by 山口仁志 on 2018/08/05.
+//  Copyright © 2018年 LightSaberApp.hitoshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
